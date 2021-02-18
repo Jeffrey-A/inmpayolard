028b84e2061bbcbc48454cfea04a7ddd161a0a76
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2021-02-17 23:38:20 -0500

    feat(/PropertyViewScreen): display image info

--- components/PropertyViewScreen/index.js
+++ components/PropertyViewScreen/index.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function PropertyViewScreen() {
+export default function PropertyViewScreen(props) {
+  const {title, images, description} = props.route.params;
+
   return (
     <View style={styles.container}>
-      <Text>Property View</Text>
+      <Text>{title}</Text>
+      <Image style={styles.image} source={{uri: images[0]}} />
+      <Text>{description}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +19,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  image: {
+    height: 400,
+    width: '100%',
+  }
 });
