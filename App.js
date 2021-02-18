a52ab74e1a2db301c64211101e8a80fc32ebd158
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2021-02-17 23:29:53 -0500

    feat: render two dummy properties and navigate to the property view onPress

--- App.js
+++ App.js
@@ -8,13 +8,73 @@ import PropertyViewScreen from "./components/PropertyViewScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+const properties = [
+  {
+    id: 1,
+    title:
+      "EXCELENTE PROYECTO DE APARTAMENTOS EN VENTA EN SAN FRANCISCO DE MACORÍS",
+    youtube_url:
+      "https://www.youtube.com/watch?v=pPedYWqcO-o&t=10s&ab_channel=Inmobiliaria56.com",
+    thumbnail_image:
+      "https://inmobiliaria56.com/wp-content/uploads/2021/02/Hansel-IIss0000.jpg",
+    description:
+      "Estos Apartamentos te ofrecen, (Proyecto Cerrado)  en San Fco de Macoris, Ubicado en una Maravillosa zona de la Provincia Duarte, con cercanía a Prestigiosos Centros Comerciales como: Palmares Mall, Bancos (Reservas, Banco Popular, BHD), Restaurantes Modernos, Estaciones de Gasolina, Gold´s Gym, Farmacias, Colegios,  y varias opciones para la recreación Familiar (A solo minutos en automóvil).",
+    images: [
+      "https://inmobiliaria56.com/wp-content/uploads/2021/02/Hansel-IIss0000.jpg",
+      "https://inmobiliaria56.com/wp-content/uploads/2021/02/Hansel-IIss0001-1.jpg",
+      "https://inmobiliaria56.com/wp-content/uploads/2021/02/Hansel-IIss0002.jpg",
+    ],
+    currency: "DOP",
+    price: 1500000,
+    type: "apartamento",
+    parking_number: 2,
+    baths_number: 1,
+    bedrooms: 3,
+    publication_date: '01-06-2020',
+    availability_date: '01-22-2021',
+    location: "Duarte",
+    pool: false,
+    square_feet: 110, 
+  },
+  {
+    id: 2,
+    title:
+      "CONVENIENTE CASA EN SAN FRANCISCO DE MACORÍS",
+    youtube_url:
+      "https://www.youtube.com/watch?v=pPedYWqcO-o&t=10s&ab_channel=Inmobiliaria56.com",
+    thumbnail_image:
+      "https://inmobiliaria56.com/wp-content/uploads/2021/01/IMG_4660-700x450.jpg",
+    description:
+      "Estos Apartamentos te ofrecen, (Proyecto Cerrado)  en San Fco de Macoris, Ubicado en una Maravillosa zona de la Provincia Duarte, con cercanía a Prestigiosos Centros Comerciales como: Palmares Mall, Bancos (Reservas, Banco Popular, BHD), Restaurantes Modernos, Estaciones de Gasolina, Gold´s Gym, Farmacias, Colegios,  y varias opciones para la recreación Familiar (A solo minutos en automóvil).",
+    images: [
+      "https://inmobiliaria56.com/wp-content/uploads/2021/01/IMG_4660-700x450.jpg",
+      "https://inmobiliaria56.com/wp-content/uploads/2021/01/IMG_4720.jpg",
+      "https://inmobiliaria56.com/wp-content/uploads/2021/01/IMG_4702.jpg",
+    ],
+    currency: "DOP",
+    price: 1500000,
+    type: "apartamento",
+    parking_number: 2,
+    baths_number: 1,
+    bedrooms: 3,
+    publication_date: '01-06-2020',
+    availability_date: '01-22-2021',
+    location: "Santo Domingo",
+    pool: false,
+    square_feet: 90,
+    contact: {
+      name: "Maria Polanco"
+    }
+  },
+];
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={props => <HomeScreen properties={properties} {...props} />} />
         <Stack.Screen
           name="Property View"
           component={PropertyViewScreen}
--- components/HomeScreen/index.js
+++ components/HomeScreen/index.js
@@ -1,25 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
 
-export default function HomeScreen(props) {
-  return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-      <StatusBar style="auto" />
-      <Button
-        title="Go to Property View"
-        onPress={() => props.navigation.navigate('Property View')}
-      />
-    </View>
-  );
+export default class HomeScreen extends React.Component {
+  render() {
+    const { properties, navigation } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <ScrollView>
+          {properties.map((property) => {
+            const { id, title, thumbnail_image } = property;
+
+            return (
+              <TouchableOpacity
+                key={id}
+                onPress={() => navigation.navigate("Property View", property)}
+              >
+                <Text>{title}</Text>
+                <Image
+                  style={styles.image}
+                  source={{
+                    uri:
+                      "https://inmobiliaria56.com/wp-content/uploads/2021/01/IMG_4660-700x450.jpg",
+                  }}
+                />
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 100,
+    height: 100,
   },
 });
